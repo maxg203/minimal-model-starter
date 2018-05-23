9df0f800dabb80361267f8184ba2b6638d55f51a
Max Goodridge <max.goodridge@hotmail.co.uk>
2018-05-23 21:20:40 +0100

    Simplify worker registration

--- worker.py
+++ worker.py
@@ -2,10 +2,4 @@ from celery import Celery
 from algorithm import approx
 
 app = Celery(__name__, backend='rpc://', broker='redis://redis:6379/0')
-
-@app.task
-def integrate(*args, **kwargs):
-    try:
-        return approx(*args, **kwargs)
-    except:
-        return
+integrate = app.task(approx)
